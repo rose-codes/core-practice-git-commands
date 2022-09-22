e822a07559014b8e6287da3abb277768dfebbeb0
Soleil <soleilherring@gmail.com>
2022-09-22 11:02:18 -0700

    pulled changes from rose

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ import pytest
 
 
 def always_returns_true():
-<<<<<<< HEAD
-    print ("Hi, Rose! :D")
-=======
     result = True
     return result
     
->>>>>>> 0002dc4604bd43ebd9c4deac0d2cc371149f8d42
 
 
 def test_always_returns_true():
